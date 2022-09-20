bc91ddcfdcbbcfd8add77d58a041ca4a6fe589e8
Anup Mishra <anup210891@gmail.com>
2022-09-20 06:09:33 +0530

    update9

--- CICD/configure-named-profile.sh
+++ CICD/configure-named-profile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # fail on any error
-sudo set -eu
+set -eu
 
 # configure named profile
-sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $PROFILE_NAME 
-sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $PROFILE_NAME 
-sudo aws configure set region $AWS_REGION --profile $    
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $PROFILE_NAME 
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $PROFILE_NAME 
+aws configure set region $AWS_REGION --profile $PROFILE_NAME     
 
 # verify that profile is configured
-sudo aws configure list --profile $PROFILE_NAME
+aws configure list --profile $PROFILE_NAME
